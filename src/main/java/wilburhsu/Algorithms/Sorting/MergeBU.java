aeb659823c305741eb449369861ca753c1614315
DeityMon <xwb-2008@163.com>
2019-02-14 16:42:43 +0800

    快速排序、三向切分快速排序

--- src/main/java/wilburhsu/Algorithms/Sorting/MergeBU.java
+++ src/main/java/wilburhsu/Algorithms/Sorting/MergeBU.java
@@ -13,25 +13,26 @@ public class MergeBU {
         int N = a.length;
         aux = new Comparable[N];
         for(int sz = 1;sz < N;sz = sz + sz)
-            for (int lo = 0;lo < N-sz; lo+=sz+sz)
+            for (int lo = 0;lo < N-sz; lo += sz+sz)
                 merge(a,lo,lo+sz-1,Math.min(lo+sz+sz-1,N-1));
     }
 
 
     public static void merge(Comparable[] a,int lo,int mid,int hi){
         int i = lo,j = mid + 1;
-        for(int k = lo; k < a.length; k++)
-            aux[k] = aux[k];
 
-        for(int k = lo; k < hi; k++)
+        for(int k = lo; k <= hi; k++)
+            aux[k] = a[k];
+
+        for(int k = lo; k <= hi; k++)
             if(i > mid)
                 a[k] = aux[j++];
             else if(j>hi)
                 a[k] = aux[i++];
-            else if (less(aux[i],aux[j]))
-                a[k] = aux[i++];
-            else
+            else if (less(aux[j],aux[i]))
                 a[k] = aux[j++];
+            else
+                a[k] = aux[i++];
 
     }
 
--- src/main/java/wilburhsu/Algorithms/Sorting/Quick3way.java
+++ src/main/java/wilburhsu/Algorithms/Sorting/Quick3way.java
@@ -1,43 +1,36 @@
 package wilburhsu.Algorithms.Sorting;
 
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
-/**
- * 算法2.4 自顶向下的归并排序 P171
- * */
-
-public class Merge {
-    private static Comparable[] aux;
-
+public class Quick3way {
     public static void sort(Comparable[] a){
-        aux = new Comparable[a.length];
-        sort(a,0,a.length - 1);
+        StdRandom.shuffle(a);
+        sort(a,0,a.length-1);
     }
 
     private static void sort(Comparable[] a,int lo,int hi){
         if(hi <= lo)
             return;
-        int mid = lo + (hi - lo)/2;
-        sort(a,lo,mid);
-        sort(a,mid + 1,hi);
-        merge(a,lo,mid,hi);
+        int lt = lo, i = lo + 1,gt = hi;
+        Comparable v = a[lo];
+        while (i <= gt){
+            int cmp = a[i].compareTo(v);
+            if(cmp < 0)
+                exch(a,lt++,i++);
+            else if (cmp > 0)
+                exch(a,i,gt--);
+            else
+                i++;
+        }
+        sort(a,lo,lt - 1);
+        sort(a,gt + 1,hi);
     }
 
-    public static void merge(Comparable[] a,int lo, int mid,int hi){
-        int i = lo,j = mid+ 1;
-
-        for(int k = lo; k<=hi; k++)
-            aux[k] = a[k];//将要排序的数据复制到临时数组aux
-
-        for(int k = lo;k <= hi; k++)
-            if(i > mid)
-                a[k] = aux[j++];//如果左边的元素已经遍历完，那么将右边数组剩下的元素一次复制到原来的数组
-            else if(j > hi)
-                a[k] = aux[i++];
-            else if(less(aux[j],aux[i]))
-                a[k] = aux[j++];
-            else
-                a[k] = aux[i++];
+    private static void exch(Comparable[] a,int i,int j){
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
     }
 
     private static boolean less(Comparable v,Comparable w) {
@@ -67,4 +60,5 @@ public class Merge {
         }
         show(a);
     }
+
 }
