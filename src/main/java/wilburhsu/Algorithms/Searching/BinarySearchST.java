b8c3c613949f0486fbeb0977e964b537613fe2ac
deitymon <xwb-2008@163.com>
2019-03-19 16:09:59 +0800

    顺序查找、二分查找

--- src/main/java/wilburhsu/Algorithms/Searching/BinarySearchST.java
+++ src/main/java/wilburhsu/Algorithms/Searching/BinarySearchST.java
@@ -1,6 +1,61 @@
 package wilburhsu.Algorithms.Searching;
 
-public class BinarySearchST {
+/**
+ * 算法3.2 二分查找（基于有序数组）P239
+ * */
+
+public class BinarySearchST<Key extends Comparable <Key>,Value> {
+    private Key[] keys;
+    private Value[] vals;
+    private int N;
+    public BinarySearchST(int capacity){
+        keys = (Key[]) new Comparable[capacity];
+        vals = (Value[]) new Object[capacity];
+    }
+
+    public int size(){
+        return N;
+    }
+
+    public Value get(Key key){
+        if(isEmpty())
+            return null;
+        int i = rank(key);
+        if(i < N && keys[i].compareTo(key) == 0)
+            return vals[i];
+        else
+            return null;
+    }
+
+    private boolean isEmpty(){
+        return size() == 0;
+    }
+
+    /*Todo 基于有序数组的二分查找(迭代) P241*/
+    public int rank(Key key){
+        int lo = 0,hi = N - 1;
+
+        return lo;
+    }
+
+    public void put(Key key,Value val){
+        //查找键，找到则更新值，否则创建新的元素
+        int i = rank(key);
+        if(i < N && keys[i].compareTo(key) == 0){
+            vals[i] = val;
+            return;
+        }
+        //在位置i插入元素，从i开始，将所有更大的键向后移动一位（从N到i，由后向前，依次移动）
+        for(int j = N; j > i;j--){
+            keys[j] = keys[j-1];
+            vals[j] = vals[j-1];
+        }
+        keys[i] = key;
+        vals[i] = val;
+        N++;
+    }
 
 
+
+    /*Todo 基于二分查找的有序符号表的其他操作*/
 }
--- src/main/java/wilburhsu/Algorithms/Searching/SequentialSearchST.java
+++ src/main/java/wilburhsu/Algorithms/Searching/SequentialSearchST.java
@@ -1,4 +1,63 @@
 package wilburhsu.Algorithms.Searching;
 
-public class SequentialSearchST {
+/**
+ * 算法3.1 顺序查找（基于无序链表）P236
+ * */
+
+public class SequentialSearchST<Key,Value> {
+    private Node first; //链表首结点
+
+    private class Node{
+        Key key;
+        Value val;
+        Node next;
+        public Node(Key key,Value val,Node next){
+            this.key = key;
+            this.val = val;
+            this.next = next;
+        }
+
+        public Value get(Key key){
+            for(Node x = first;x != null;x = x.next)
+                if(key.equals(x.key))
+                    return x.val;
+                return null;
+        }
+
+        public void put(Key key,Value val){
+            for(Node x = first;x != null; x = x.next)
+                if(key.equals(x.key)){
+                    x.val = val;
+                    return;
+                }
+            first = new Node(key,val,first); //未命中，新建结点
+        }
+
+        //Todo size()方法
+        public int size(){
+
+            return 0;
+        }
+
+        //Todo key()方法
+        public Iterable<Key> key(){
+
+            return null;
+        }
+
+        //TODO delete()方法
+        public void delete(Key key){
+            if(key == null)
+                throw new IllegalArgumentException("key s null in function delete");
+
+            Node fakehead = new Node(null,null,first);
+
+            for(Node prev = fakehead;prev.next != null;prev = prev.next)
+                if(key.equals(prev.next.key)){
+                    prev.next = prev.next.next;
+                    break;
+                }
+            first = fakehead.next;
+        }
+    }
 }
