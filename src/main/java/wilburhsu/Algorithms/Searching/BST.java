836a1beabf6061dffae7e9aac0bd2807b76cef94
deitymon <xwb-2008@163.com>
2019-03-21 09:57:06 +0800

    二叉查找树的查找和排序方法实现

--- src/main/java/wilburhsu/Algorithms/Searching/BST.java
+++ src/main/java/wilburhsu/Algorithms/Searching/BST.java
@@ -10,7 +10,7 @@ public class BST <Key extends Comparable<Key>,Value>{
         private Key key;
         private Value val;
         private Node left,right;
-        private int N;
+        private int N; //以该结点为根的子树中的结点总数
 
         public Node(Key key,Value val,int N){
             this.key = key;
@@ -32,12 +32,42 @@ public class BST <Key extends Comparable<Key>,Value>{
 
     /*二叉查找树的查找和排序方法的实现 P252*/
     public Value get(Key key){
+        return get(root,key);//该方法从根结点开始查找
+    }
 
-        return null;
+    private Value get(Node x,Key key){
+        //在以x为根结点的子树中查找并返回key所对应的值
+        //如果找不到则返回null
+        if(x == null)
+            return null;
+        int cmp = key.compareTo(x.key);
+        if(cmp < 0)
+            return get(x.left,key);
+        else if(cmp > 0)
+            return get(x.right,key);
+        else
+            return x.val;
     }
 
     public void put(Key key,Value val){
+        //查找key，找到则更新它的值，否则为它创建一个新的结点
+        root = put(root,key,val);
+    }
 
+    private Node put(Node x,Key key,Value val){
+        //如果key存在于以x为根结点的子树中则更新它的值
+        //否则将以key和val为键值对的新结点插入到该子树中
+        if(x == null)
+            return new Node(key,val,1);
+        int cmp = key.compareTo(x.key);
+        if(cmp < 0)
+            x.left = put(x.left,key,val);
+        if(cmp > 0)
+            x.right = put(x.right,key,val);
+        else
+            x.val = val;
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
     }
 
 
