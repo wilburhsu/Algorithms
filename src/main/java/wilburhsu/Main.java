3e9e2163fb34c2e457566efda5f2e7f710453dca
DeityMon <xwb-2008@163.com>
2019-08-28 22:17:24 +0800

    面试秘籍

--- src/main/java/wilburhsu/Main.java
+++ src/main/java/wilburhsu/Main.java
@@ -1,5 +1,30 @@
 package wilburhsu;
 
-public class Main {
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
 
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Main {
+    public static void swap(TreeNode node1,TreeNode node2){
+        TreeNode tmp = node1;
+        node1 = node2;
+        node2 = tmp;
+        System.out.println("node1:" + node1.val);
+        System.out.println("node2:" + node2.val);
+    }
+
+    public static void main(String[] args) {
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(2);
+        swap(a,b);
+
+        System.out.println("a:" + a.val);
+        System.out.println("b:" + b.val);
+    }
 }
--- src/main/java/wilburhsu/Solution.java
+++ src/main/java/wilburhsu/Solution.java
@@ -6,19 +6,30 @@ import java.util.Stack;
 
 
 public class Solution {
-    private static Stack<String> stack = new Stack<>();
-    private static ArrayList list = new ArrayList();
-    public static void testPush(){
-        stack.push(null);
-        stack.push("1");
+    public int reverse(int x) {
+        if(x < Integer.MIN_VALUE || x > Integer.MAX_VALUE)
+            return 0;
+        while(x%10 == 0)
+            x = x/10;
+        String number = String.valueOf(x);
+        char[] chars = number.toCharArray();
+        int i = 0;
+        if(chars[0] == '+' || chars[0] == '-')
+            i = 1;
+        int j = chars.length - 1;
+        char tmp = ' ';
+        while(i <= j){
+            tmp = chars[i];
+            chars[i] = chars[j];
+            chars[j] = tmp;
+            i++;
+            j--;
+        }
+        return Integer.valueOf(String.valueOf(chars)).intValue();
     }
 
     public static void main(String[] args) {
-        Solution.testPush();
-        System.out.println(stack.size());
-//        while(!stack.empty())
-//            System.out.println(stack.pop());
-        stack.pop();
-        System.out.println(stack.pop());
+        Solution solution = new Solution();
+        System.out.println(solution.reverse(123000));
     }
 }
