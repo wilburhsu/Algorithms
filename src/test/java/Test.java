abebf764ac26a7ccbeeff7e28999633d2d7eaf24
DeityMon <xwb-2008@163.com>
2019-09-04 00:16:21 +0800

    finally中return

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,5 +1,25 @@
 public class Test {
+    private int hasErrorAndFinally() {
+        try {
+            System.out.println("try代码块已执行");
+            int i = 1 / 0;
+            System.out.println("i = " + i);
+            System.out.println("try代码块已结束");
+            return 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("catch代码块已执行");
+            System.out.println("catch代码块已结束");
+            return 2;
+        } finally {
+            System.out.println("finally代码块已执行");
+            System.out.println("finally代码块已结束");
+            return 3;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(2 & 1);
+        Test test = new Test();
+        System.out.println(test.hasErrorAndFinally());
     }
 }
