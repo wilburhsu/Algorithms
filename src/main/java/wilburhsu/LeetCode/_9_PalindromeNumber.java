482433b31ae4e64f3b9e3e95ab360c587f483c2d
DeityMon <xwb-2008@163.com>
2019-09-06 09:19:56 +0800

    ==和equals()区别

--- src/main/java/wilburhsu/LeetCode/_9_PalindromeNumber.java
+++ src/main/java/wilburhsu/LeetCode/_9_PalindromeNumber.java
@@ -1,6 +1,16 @@
 package wilburhsu.LeetCode;
 
 public class _9_PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        if(x < 0)
+            return false;
+        int div = 1;
+        while(x/div >= 10)
+            div *= 10;
+        while (x > 0){
 
+        }
 
+        return true;
+    }
 }
