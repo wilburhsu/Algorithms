e543e7614c3648858845e68380a0bde91b8a8c05
DeityMon <xwb-2008@163.com>
2019-09-24 00:14:40 +0800

    20_有效括号

--- src/main/java/wilburhsu/LeetCode/_20_ValidParentheses.java
+++ src/main/java/wilburhsu/LeetCode/_20_ValidParentheses.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import java.util.Stack;
 
 public class _20_ValidParentheses {
-
     /**位运算判断奇偶
      **if(i & 1){
      *    //i是奇数情况执行的代码
@@ -19,9 +18,21 @@ public class _20_ValidParentheses {
             return true;
         if((s.length() & 1) == 1)
             return false;
+        Map<Character,Character> map = new HashMap<>();
+        map.put(')','(');
+        map.put(']','[');
+        map.put('}','{');
+
         Stack<Character> stack = new Stack<>();
         for(int i=0;i < s.length();i++){
-
+            char c = s.charAt(i);
+            if(map.containsKey(c)){
+                char topElement = stack.isEmpty() ?'#':stack.pop();
+                if(topElement != map.get(c))
+                    return false;
+            }else {
+                stack.push(c);
+            }
         }
 
         return stack.isEmpty();
