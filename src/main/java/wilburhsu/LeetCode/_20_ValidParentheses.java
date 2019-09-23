f6849950ae7ee52621f8bb74d2c987825a1cef4d
DeityMon <xwb-2008@163.com>
2019-09-23 16:59:16 +0800

    20_括号的有效性（未完成）

--- src/main/java/wilburhsu/LeetCode/_20_ValidParentheses.java
+++ src/main/java/wilburhsu/LeetCode/_20_ValidParentheses.java
@@ -1,8 +1,29 @@
 package wilburhsu.LeetCode;
 
-public class _20_ValidParentheses {
-    public boolean isValid(String s) {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 
-        return true;
+public class _20_ValidParentheses {
+
+    /**位运算判断奇偶
+     **if(i & 1){
+     *    //i是奇数情况执行的代码
+     *}
+     *else{
+     *    //i是偶数情况执行的代码
+     *}
+     */
+    public boolean isValid(String s) {
+        if(s.length() == 0)
+            return true;
+        if((s.length() & 1) == 1)
+            return false;
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i < s.length();i++){
+
+        }
+
+        return stack.isEmpty();
     }
 }
