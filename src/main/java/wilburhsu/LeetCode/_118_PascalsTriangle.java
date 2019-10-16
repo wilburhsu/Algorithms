d54e7e6ca518ed3037afbce84231694a2145c58e
DeityMon <xwb-2008@163.com>
2019-10-16 13:44:57 +0800

    88_合并有序数据简洁写法

---
+++ src/main/java/wilburhsu/LeetCode/_118_PascalsTriangle.java
@@ -0,0 +1,17 @@
+package wilburhsu.LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _118_PascalsTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> firstRow = new ArrayList<>();
+        firstRow.add(1);
+        result.add(firstRow);
+        for(int i = 0;i < numRows;i++){
+
+        }
+        return null;
+    }
+}
