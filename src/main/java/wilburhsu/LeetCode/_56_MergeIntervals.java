8c70d492b9b0278e825115730f1d7d41ff30c782
DeityMon <xwb-2008@163.com>
2019-11-15 00:25:13 +0800

    56_合并区间（未写完）

--- src/main/java/wilburhsu/LeetCode/_56_MergeIntervals.java
+++ src/main/java/wilburhsu/LeetCode/_56_MergeIntervals.java
@@ -1,8 +1,25 @@
 package wilburhsu.LeetCode;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class _56_MergeIntervals {
     public int[][] merge(int[][] intervals) {
-        
-        return  null;
+        if(intervals == null || intervals.length == 0)
+            return intervals;
+        Arrays.sort(intervals, new Comparator<int[]>(){
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        int low = intervals[0][0];
+        int high = intervals[0][1];
+        for(int i = 1;i < intervals.length;++i){
+            
+        }
+
+
+        return null;
     }
 }
