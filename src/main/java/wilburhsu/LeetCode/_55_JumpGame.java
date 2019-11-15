279b3fa26ecf09f0d71edec0f4860d43f10f9465
DeityMon <xwb-2008@163.com>
2019-11-15 09:25:41 +0800

    55_跳跃游戏

---
+++ src/main/java/wilburhsu/LeetCode/_55_JumpGame.java
@@ -0,0 +1,12 @@
+package wilburhsu.LeetCode;
+
+public class _55_JumpGame {
+    public boolean canJump(int[] nums){
+        int max = 0;
+        for(int i = 0;i < nums.length;++i){
+            if(i > max)
+                return false;
+        }
+        return true;
+    }
+}
