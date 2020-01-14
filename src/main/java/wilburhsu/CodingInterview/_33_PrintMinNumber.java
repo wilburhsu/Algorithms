950e8341c913902b6dc4afad423c4ac4ba5b3e03
DeityMon <xwb-2008@163.com>
2020-01-14 16:48:22 +0800

    剑指offer第33题注释，第44题

--- src/main/java/wilburhsu/CodingInterview/_33_PrintMinNumber.java
+++ src/main/java/wilburhsu/CodingInterview/_33_PrintMinNumber.java
@@ -9,8 +9,9 @@ import java.util.Comparator;
  * */
 public class _33_PrintMinNumber {
     public String printMinNumber(int[] numbers) {
-        if(numbers.length <= 0)
-            return null;
+//        if(numbers.length <= 0)
+//            return null;//❌返回null无法通过所有测试用例
+        //长度为0应该返回"";
 
         StringBuffer buffer = new StringBuffer();
         int length = numbers.length;
@@ -20,9 +21,14 @@ public class _33_PrintMinNumber {
         //自定义比较器
         Arrays.sort(strArray,new Comparator<String>(){
             @Override
-            public int compare(String o1, String o2) {
-                String c1 = o1 + o2;
-                String c2 = o2 + o1;
+            /* 按字典序对c1，c2进行排序
+            *  若c1排在c2之后（即c1 > c2，ab > ba），则返回1，那么a > b也即a排在b之后
+            *  同理若c1排在c2之前（即c1 < c2，ab < ba），则返回0，那么a < b也即a排在b之前
+            *  举例：a=321,b=32，ab=32132,ba=32321,ab<ba,则a排在b前面
+            * */
+            public int compare(String a, String b) {
+                String c1 = a + b;
+                String c2 = b + a;
                 return c1.compareTo(c2);
             }
         });
--- src/main/java/wilburhsu/CodingInterview/_44_ContinousCards.java
+++ src/main/java/wilburhsu/CodingInterview/_44_ContinousCards.java
@@ -11,7 +11,7 @@ public class _44_ContinousCards {
         for (int i = 0; i < numbers.length - 1; i++) {
             if (numbers[i] == 0) {
                 countOfZero++;
-                continue;
+                continue;//例如输入{0,0,1,2,3}，如果不加continue，就会把0==0当成对子处理，直接返回false;
             }
             if (numbers[i] == numbers[i + 1])
                 return false;
@@ -21,5 +21,23 @@ public class _44_ContinousCards {
         if (countOfZero >= countOfGap)
             return true;
         return false;
+    }
+
+    public boolean isContinuous2(int [] numbers) {
+        if(numbers.length != 5)
+            return false;
+
+        int[] count = new int[14];
+        int max = -1;
+        int min = 14;
+        for(int number:numbers){
+            if(number == 0)
+                continue;
+            if(++count[number] > 1)
+                return false;
+            max = Math.max(number,max);
+            min = Math.min(number,min);
+        }
+        return max - min < 5;
     }
 }
